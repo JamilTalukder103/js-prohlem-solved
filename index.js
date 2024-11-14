6d0c743aaa232bc83997748fb43d34d55f31ae34
JamilTalukder103 <mdjamiltalukder143@gmail.com>
2024-11-15 03:08:12 +1300

    even number and odd number check

---
+++ index.js
@@ -0,0 +1,9 @@
+function oddNumber(number){
+    if(number%2===0){
+        return 'this a odd number'
+    }else{
+        return 'this is a even number'
+    }
+}
+const result=oddNumber(9)
+console.log(result)
